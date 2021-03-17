021c4337215b824c28311a92a32a26806fd7a024
dgoldsb <dgoldsb@live.nl>
2021-03-17 15:32:16 +0100

    Day 9 part 2

--- days/src/day_09.rs
+++ days/src/day_09.rs
@@ -85,7 +85,63 @@ fn part_a(input: &Vec<char>) -> usize {
     return uncompressed.len();
 }
 
+fn find_length_recursively(mut queue: VecDeque<char>) -> usize {
+    let mut counter: usize = 0;
+
+    // Reserve a bit of memory for the current marker.
+    let mut current_marker: Marker = Marker::new();
+
+    while queue.len() > 0 {
+        let char = queue.pop_front().unwrap();
+        match char {
+            MARKER_START => current_marker.phase = MarkerState::WidthDefining,
+            MARKER_SEP => current_marker.phase = MarkerState::MultiplierDefining,
+            MARKER_END => {
+                current_marker.phase = MarkerState::Finished;
+
+                // Process the marker.
+                let mut subqueue: VecDeque<char> = VecDeque::new();
+                for _ in 0..current_marker
+                    .width
+                    .iter()
+                    .collect::<String>()
+                    .parse()
+                    .unwrap()
+                {
+                    subqueue.push_back(queue.pop_front().unwrap());
+                }
+                let multiplier: usize = current_marker
+                    .multiplier
+                    .iter()
+                    .collect::<String>()
+                    .parse()
+                    .unwrap();
+                counter += find_length_recursively(subqueue) * multiplier;
+
+                current_marker = Marker::new();
+            }
+            _ => match current_marker.phase {
+                MarkerState::Unstarted => counter += 1,
+                MarkerState::WidthDefining => current_marker.width.push(char),
+                MarkerState::MultiplierDefining => current_marker.multiplier.push(char),
+                MarkerState::Finished => counter += 1,
+            },
+        }
+    }
+
+    return counter;
+}
+
+fn part_b(input: &Vec<char>) -> usize {
+    // Turn into a dequeue of characters.
+    let mut queue: VecDeque<char> = VecDeque::new();
+    queue.extend(input);
+
+    return find_length_recursively(queue);
+}
+
 pub fn day_09() {
     let input = parse_chars("day_09".to_string());
     println!("A: {}", part_a(&input));
+    println!("B: {}", part_b(&input));
 }
